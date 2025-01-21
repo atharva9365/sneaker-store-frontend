b0de055caa81a48a251b14a4dc9d8059c597df43
atharva9365 <kolteatharva1@gmail.com>
2025-01-21 12:44:48 +0530

    fixed hero section

--- src/components/HeroVideoSection.jsx
+++ src/components/HeroVideoSection.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
+import React from 'react';
 import video from "../assets/nikevideo.mp4";
-
-
 
 const HeroVideoSection = () => {
   return (
-    <>
-      <div className="hero-video-section mt-1"> {/* Add margin-top using mt-8 */}
-        <div className="video-section flex justify-center items-center">
-          <video
-            src={video}
-            autoPlay
-            loop
-            muted
-            playsInline
-            className="w-full h-[600px] md:h-[600px] lg:h-[550px]" 
-          >
-            Your browser does not support the video tag.
-          </video>
-        </div>
+    <div className="hero-video-section mt-1">
+      <div className="video-section flex justify-center items-center">
+        <video
+          src={video}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full h-auto max-h-[600px] sm:max-h-[500px] md:max-h-[600px] lg:max-h-[550px] object-cover"
+        >
+          Your browser does not support the video tag.
+        </video>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default HeroVideoSection
+export default HeroVideoSection;
